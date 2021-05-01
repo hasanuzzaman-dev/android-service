536231c350ef8a1c82b237fa942cedf02ce60c47
hasanuzzman-dev <hasanuzzaman.dev@gmail.com>
2021-05-02 02:42:41 +0600

    intent service done

--- app/src/main/java/com/hasan/androidservice/MyIntentService.java
+++ app/src/main/java/com/hasan/androidservice/MyIntentService.java
@@ -2,6 +2,10 @@ package com.hasan.androidservice;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
@@ -12,6 +16,7 @@ import android.content.Intent;
 public class MyIntentService extends IntentService {
 
 
+    private static final String TAG = "MyIntentService";
 
     public MyIntentService() {
         super("MyIntentService");
@@ -19,7 +24,30 @@ public class MyIntentService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        Log.d(TAG, "onHandleIntent: service started");
+        // Here writing the code
+        // intent service run on the background thread but service run on the main UI thread
+        // here we can not interact with UI like we can not Toast here. But we use handler
+        // here need main looper
 
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MyIntentService.this, "onHandleIntent method", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate: service created");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: service destroyed");
+    }
 }
