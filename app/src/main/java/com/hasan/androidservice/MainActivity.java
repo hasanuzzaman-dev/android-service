daa97f53a71e0d9e7fcd0f8a0caa22f0d5bd4aa9
hasanuzzman-dev <hasanuzzaman.dev@gmail.com>
2021-05-02 15:39:35 +0600

    MyBoundService create how to call MyBoundService method in other class

--- app/src/main/java/com/hasan/androidservice/MainActivity.java
+++ app/src/main/java/com/hasan/androidservice/MainActivity.java
@@ -1,15 +1,35 @@
 package com.hasan.androidservice;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.hasan.androidservice.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private MyBoundService myBoundService = null;
+    private boolean isBound = false;
+
+    ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            MyBoundService.MyLocalBinder binder = (MyBoundService.MyLocalBinder) iBinder;
+            myBoundService = binder.getMyBoundService();
+            isBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Services
         binding.startServiceBtn.setOnClickListener(view -> {
-            startService(new Intent(this,MyService.class));
+            startService(new Intent(this, MyService.class));
         });
 
         binding.stopServiceBtn.setOnClickListener(view -> {
@@ -28,9 +48,31 @@ public class MainActivity extends AppCompatActivity {
 
         // Intent Services
         binding.startMyIntentService.setOnClickListener(view -> {
-            startService(new Intent(this,MyIntentService.class));
+            startService(new Intent(this, MyIntentService.class));
+        });
+
+        binding.showRandomNumberBtn.setOnClickListener(view -> {
+            if (isBound){
+                int random = myBoundService.generateRandomNumber();
+                Toast.makeText(myBoundService, "random: "+random, Toast.LENGTH_SHORT).show();
+            }
         });
 
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, MyBoundService.class);
+        bindService(intent, connection, BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (isBound){
+            unbindService(connection);
+        }
     }
 }
